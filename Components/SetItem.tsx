76146a868a1319df65639fbfe3440282ef493eec
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2021-02-07 16:32:30 -1000

    add files to render card sets

---
+++ Components/SetItem.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+interface SetItemProps {}
+
+export const SetItem: React.FC<SetItemProps> = ({}) => {
+  return <Text>Set Item</Text>;
+};
---
+++ Components/Sets.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+interface SetsProps {}
+
+export const Sets: React.FC<SetsProps> = ({}) => {
+  return (
+    <SafeAreaView>
+      <Text>Sets</Text>
+    </SafeAreaView>
+  );
+};
