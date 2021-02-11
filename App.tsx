c1ff1f55e68363fb165b25c193e9a8ed2b3f94d5
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2021-02-11 01:53:07 -1000

    add styled-components

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 import Sets from './Components/Sets';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
@@ -9,20 +8,10 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Tab.Navigator initialRouteName={'Sets'}>
-          <Tab.Screen name="Sets" component={Sets} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName={'Sets'}>
+        <Tab.Screen name="Sets" component={Sets} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
-});
