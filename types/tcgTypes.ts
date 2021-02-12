b1660ed48397d27bea66187b6e90d4b2281ee5b4
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2021-02-12 02:30:30 -1000

    added types for individual cards

--- types/tcgTypes.ts
+++ types/tcgTypes.ts
@@ -1,3 +1,4 @@
+// Sets
 export interface CardSet {
   id: string;
   name: string;
@@ -24,4 +25,52 @@ export interface CardSetHash {
 export interface CardSetData {
   title: string;
   data: CardSet[][];
+}
+
+// Cards
+export interface Card {
+  id: string;
+  name: string;
+  supertype: string;
+  subtypes: string[];
+  hp: string;
+  types: string[];
+  evolvesTo: string[];
+  attacks: IAttack[];
+  weaknesses: IWeakness[];
+  retreatCost: string[];
+  convertedRetreatCost: number;
+  set: CardSet;
+  images: {
+    small: string;
+    large: string;
+  };
+  tcgplayer: {
+    url: string;
+    updatedAt: string;
+    prices: {
+      [key: string]: CardPrice;
+    };
+  };
+}
+
+export interface IAttack {
+  convertedEnergyCost: number;
+  cost: string[];
+  damage: string;
+  name: string;
+  text: string;
+}
+
+export interface IWeakness {
+  type: string;
+  value: string;
+}
+
+export interface CardPrice {
+  low: number;
+  mid: number;
+  high: number;
+  market: number;
+  directLow: number;
 }
