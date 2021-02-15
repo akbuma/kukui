a2a0080e5104b90a9cceebc1128df099b3a3f527
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2021-02-14 23:43:10 -1000

    add types for set navigation

---
+++ Components/Sets/SetsNavigator.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+
+interface SetsNavigatorProps {
+
+}
+
+const Stack = createStackNavigator();
+
+export const SetsNavigator: React.FC<SetsNavigatorProps> = ({}) => {
+    return ();
+}
---
+++ Components/Sets/setsParamList.ts
@@ -0,0 +1,13 @@
+import { RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+export type SetStackParamList = {
+  Sets: undefined;
+  AllCards: undefined;
+  Card: undefined;
+};
+
+export type SetStackNavProps<T extends keyof SetStackParamList> = {
+  navigation: StackNavigationProp<SetStackParamList, T>;
+  route: RouteProp<SetStackParamList, T>;
+};
